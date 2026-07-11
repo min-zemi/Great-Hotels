e2867c93ecefcb862699ad0153d65c31ff3a2a69
min-zemi <pengbianquanji@gmail.com>
2026-07-11 13:49:53 +0900

    Implement the first basic view functions

--- hotels/views.py
+++ hotels/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Hotel Booking Home")
+
+def hotel_list(request):
+    return HttpResponse("Matching Hotels")
+
+def register(request):
+    return HttpResponse("User Registration")
+
+def reserve(request):
+    return HttpResponse("Reservation Completed")
+
+def success(request):
+    return HttpResponse("Booking Success")
